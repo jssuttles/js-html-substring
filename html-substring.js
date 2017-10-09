e8bea117e1668afad87aec00f2321930b8b0e6ef
Jason Suttles <jasonssuttles@gmail.com>
2017-10-09 15:18:12 -0400

    Update html-substring.js
    check that module is not undefined

--- html-substring.js
+++ html-substring.js
@@ -67,6 +67,6 @@ function html_substr(src, length, suffix){
     if(suffix === false) return str;
     return str + ((typeof suffix == "undefined" || suffix === true) ? "..." : suffix);
 }
-if (module && module.exports) {
+if (typeof module !== 'undefined' && module.exports) {
     module.exports = html_substr;
 }
